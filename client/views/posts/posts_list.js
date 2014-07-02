74e1d51ac90c5456c67a695e7607325f736c7526
Abraham R Burnett <abeburnett@gmail.com>
2014-07-02 00:31:26 -0700

    wire collection into postsList template

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,21 +1,5 @@
-var postsData = [
-	{
-		title: 'Writing a book, my way',
-		author: 'Abe Fox',
-		url: 'http://abefox.com/'
-	},
-	{
-		title: 'When the writing gets tough',
-		author: 'Abe Fox',
-		url: 'http://abefox.com'
-	},
-	{
-		title: 'An update on The Unchosen',
-		author: 'Abe Fox',
-		url: 'http:///abefox.com'
-	}
-];
-
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() {
+		return Posts.find();
+	}
 });
