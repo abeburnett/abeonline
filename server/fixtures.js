2dffa46a337ce7069840661be52140392c9f2d0d
Abraham R Burnett <abeburnett@gmail.com>
2014-07-07 23:10:48 -0700

    add accounts and template to header

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,18 +2,21 @@ if (Posts.find().count() === 0) {
 	Posts.insert({
 		title: 'Writing as a way of life',
 		author: 'Abe Fox',
-		url: 'http://abefox.com'
+		url: 'http://abefox.com',
+		postText: 'This is a post about writing as a way of life.'
 	});
 
 	Posts.insert({
 		title: 'The phases of writing a novel',
 		author: 'Abe Fox',
-		url: 'http://abefox.com'
+		url: 'http://abefox.com',
+		postText: 'This is a post about the phases of writing a novel.'
 	});
 
 	Posts.insert({
 		title: 'Getting through blocks',
 		author: 'Abe Fox',
-		url: 'http://abefox.com'
+		url: 'http://abefox.com',
+		postText: "This is a post about getting through writer's block."
 	});
 }
